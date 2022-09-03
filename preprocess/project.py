c9cea6313bdf2ec255c8b38f71829a942b3e89a0
TB5zhh <tianbeiwen88@gmail.com>
2022-09-03 17:22:55 +0800

    finish projection

--- preprocess/project.py
+++ preprocess/project.py
@@ -1,18 +1,22 @@
 # %%
+from ctypes.wintypes import RGB
 import os
 from pathlib import Path
+from tkinter import N
 import numpy as np
 import cv2
 import re
 from plyfile import PlyData, PlyElement
 from IPython import embed
-
+from tqdm import tqdm
 from stairseg.utils.transform import translation_mat, rotation_mat
 
 RGB_DIR = '../data/stairs/data/mono/rgb'
 DEPTH_DIR = '../data/stairs/data/mono/depth'
 MASK_DIR = '../data/annotations/SegmentationClass'
 SAMPLING_LOG_PATH = '../data/stairs/log/sampling_log.yaml'
+PLY_SAVE_DIR = '../data/plydata'
+PLY_SAVE_VIS_DIR = '../data/plydata_vis'
 
 parse_rgb = lambda path: cv2.imread(path, cv2.IMREAD_COLOR)
 parse_depth = lambda path: cv2.imread(path, cv2.IMREAD_UNCHANGED)
@@ -71,7 +75,7 @@ def construct_vertex_ply(coords: np.ndarray, rgb: np.ndarray, label: np.ndarray,
     return PlyData([el], text=not binary)
 
 
-def project(rgb, depth, mask, fov, cam_rot, cam_coord) -> PlyData:
+def project(rgb, depth, mask, fov, cam_rot, cam_coord, visualize_mask=False) -> PlyData:
     height, width, _ = rgb.shape
     horizontal_ratio = np.array([(2. * i / width - 1) for i in range(width)])  # [-1, 1]
     horizontal_ratio_mat = np.stack([horizontal_ratio for _ in range(height)], axis=1).T
@@ -80,36 +84,34 @@ def project(rgb, depth, mask, fov, cam_rot, cam_coord) -> PlyData:
     x_mat = horizontal_ratio_mat * np.tan(fov[0] / 2 / 180 * np.pi) * depth
     z_mat = vertical_ratio_mat * np.tan(fov[1] / 2 / 180 * np.pi) * depth
     y_mat = depth
-    aff_coords = np.stack([x_mat, y_mat, z_mat, np.ones_like(x_mat)],
-                          axis=2) @ translation_mat(*[-i for i in cam_coord]) @ rotation_mat(*[-i / 180 * np.pi for i in cam_rot])
-
+    aff_coords = np.stack(
+        [x_mat, y_mat, z_mat, np.ones_like(x_mat)],
+        axis=2,
+    ) @ translation_mat(*[-i for i in cam_coord]) @ rotation_mat(*[-i / 180 * np.pi for i in cam_rot])
     coords = aff_coords[:, :, :3]
-    embed()
-    construct_vertex_ply(coords, rgb, mask).write('test.ply')
-    # with open('test.txt', 'w') as f:
-    #     for coord, rgb, mask_bit in zip(coords.reshape(-1, 3), rgb.reshape(-1, 3), mask.flatten()):
-    #         construct_vertex_ply(coord, rgb, mask_bit)
+    if visualize_mask:
+        rgb = np.stack([mask for _ in range(3)], axis=2).astype(np.uint8) * 255
+    return construct_vertex_ply(coords, rgb, mask)
 
 
 if __name__ == '__main__':
-    sorted_ls = lambda path: [path + '/' + i for i in sorted(os.listdir(path))]
-    for rgb_path, depth_path, mask_path, (fov, _, rot,
-                                          coord) in zip(sorted_ls(RGB_DIR), sorted_ls(DEPTH_DIR), sorted_ls(MASK_DIR), parse_log(SAMPLING_LOG_PATH)):
+    os.makedirs(PLY_SAVE_DIR, exist_ok=True)
+    os.makedirs(PLY_SAVE_VIS_DIR, exist_ok=True)
+    sorted_ls = lambda path: [(i, path + '/' + i) for i in sorted(os.listdir(path))]
+    sorted_ls_anno = lambda path: [(
+        f'{i:03d}.png' if f'{i:03d}.png' in os.listdir(path) else None,
+        path + '/' + f'{i:03d}.png',
+    ) for i in range(len(sorted_ls(RGB_DIR)))]
+    for (rgb_name, rgb_path), (depth_name, depth_path), (mask_name, mask_path), (fov, _, rot, coord) in zip(
+            tqdm(sorted_ls(RGB_DIR)), sorted_ls(DEPTH_DIR), sorted_ls_anno(MASK_DIR), parse_log(SAMPLING_LOG_PATH)):
+        if mask_name is None:
+            continue
+        assert rgb_name.split('.')[0] == depth_name.split('.')[0]
+        assert rgb_name.split('.')[0] == mask_name.split('.')[0]
         rgb = parse_rgb(rgb_path)
         depth = parse_depth(depth_path)
         mask = parse_mask(mask_path)
-        project(rgb, depth, mask, fov, rot, coord)
-        break
-
-# %%
-
-### DEBUG ONLY ###
-import numpy as np
-import cv2
-from stairseg.utils.vis import arr2Image
-
-PFM_PATH = '../data/stairs/data/mono/depth/000.pfm'
-
-im = cv2.imread(PFM_PATH, cv2.IMREAD_UNCHANGED)
+        project(rgb, depth, mask, fov, rot, coord).write(Path(PLY_SAVE_DIR) / ('.'.join(rgb_name.split('.')[:-1]) + '.ply'))
+        project(rgb, depth, mask, fov, rot, coord, visualize_mask=True).write(Path(PLY_SAVE_VIS_DIR) / ('.'.join(rgb_name.split('.')[:-1]) + '.ply'))
 
 # %%
